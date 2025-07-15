97a59399b1ec07c2ec9b3a1428392875a9c8dc3c
Matheus Marcelo <matheusmpmenezes@protonmail.com>
2025-07-15 12:03:07 -0300

    Create spotdl.sh

---
+++ spotdl.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install python3 python3-pip
+pip install spotdl
+
+
+Se quiser evitar conflito com outros pacotes, pode usar um ambiente virtual (python3 -m venv env && source env/bin/activate).
+⚙️ Extra (opcional): usar com ffmpeg para conversões
+sudo apt update
+sudo apt install ffmpeg
+
+
+spotdl https://open.spotify.com/playlist/SEU_LINK_AQUI
+
+
