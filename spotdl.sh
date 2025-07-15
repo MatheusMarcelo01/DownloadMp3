f47757746b9795ce9be2ceee2877d46b3dcba2b8
Matheus Marcelo <matheusmpmenezes@protonmail.com>
2025-07-15 12:03:34 -0300

    Update spotdl.sh

--- spotdl.sh
+++ spotdl.sh
@@ -3,7 +3,10 @@ sudo apt install python3 python3-pip
 pip install spotdl
 
 
-Se quiser evitar conflito com outros pacotes, pode usar um ambiente virtual (python3 -m venv env && source env/bin/activate).
+pelo Ambiente virtual: 
+(python3 -m venv env && source env/bin/activate).
+
+
 ⚙️ Extra (opcional): usar com ffmpeg para conversões
 sudo apt update
 sudo apt install ffmpeg
